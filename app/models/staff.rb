d6baaa7a051931b95fa156a0dca04f3df23860ca
gauravmahajan31 <gaurav@vinsol.com>
2014-11-14 15:25:02 +0530

    Issue fix - staff password confirmation

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,7 +2,8 @@ class Staff < User
   validates :designation, presence: true
   validates :services, presence: true
   validates :password, presence: :true, if: :should_validate_password?
-  validates :password, format: { with: PASSWORD_VALIDATOR_REGEX, message: 'can not include spaces.' }, if: :should_validate_password?
+  validates :password, confirmation: true, if: :encrypted_password_changed?
+  validates :password, format: { with: PASSWORD_VALIDATOR_REGEX, message: 'can not include spaces.' }, if: :encrypted_password_changed?
   
   has_many :allocations
   has_many :services, through: :allocations
@@ -16,14 +17,6 @@ class Staff < User
   end
 
   def should_validate_password?
-    !encrypted_password && !new_record?
-  end
-
-  # TODO: This method should not be present. Validations should not be tested in a regular method.
-  def password_match?
-    self.errors[:password] << "can't be blank" if password.blank?
-    self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
-    self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
-    password == password_confirmation && !password.blank?
+    encrypted_password.empty? && !new_record?
   end
 end
