d0eade6e5e97c2be981af92729acb7b3d2b597ce
Gaurav <gaurav@vinsol.com>
2014-11-24 12:47:10 +0530

    Add service select box

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,7 +1,6 @@
 class AvailabilitiesController < ApplicationController
   def index
     @availabilities = Availability.all
-    # @availabilities = Availability.select("staff_id,start_time , end_time, 'event' AS title, start_time AS start, end_time AS end").where("start_date BETWEEN '#{ params[:start] }' AND '#{ params[:end] }' OR end_date BETWEEN '#{ params[:start] }' AND '#{ params[:end] }'")
     @availability_json = []
     @availabilities.each do |availability|
       @availability_json << availability.get_full_calendar_json
