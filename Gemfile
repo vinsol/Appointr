3fdb536f3a7df6d3a78fc565777dd5ecb9aba5cc
Gaurav <gaurav@vinsol.com>
2014-11-26 19:26:57 +0530

    Issue fix

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
+# Use paperclip
+gem "paperclip", "~> 4.2"
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails', '3.1.2'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -31,7 +31,6 @@ class Staff < User
 
   def is_occupied?(start_at, end_at, date, new_appointment_id)
     appointments.any? do |appointment|
-      debugger
       if new_appointment_id
         appointment.id != new_appointment_id && appointment.start_at.to_date == date && ((start_at.seconds_since_midnight >= appointment.start_at.seconds_since_midnight && start_at.seconds_since_midnight < appointment.end_at.seconds_since_midnight) || (end_at.seconds_since_midnight > appointment.start_at.seconds_since_midnight && end_at.seconds_since_midnight <= appointment.end_at.seconds_since_midnight))
       else
