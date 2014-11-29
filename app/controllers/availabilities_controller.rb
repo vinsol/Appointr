50b3d2cfcaa9d7ced6948f9333e13e90f3894df6
Gaurav <gaurav@vinsol.com>
2014-11-29 13:52:59 +0530

    Remove past day availabilities from calendar view.

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -6,6 +6,7 @@ class AvailabilitiesController < ApplicationController
       @availabilities = Service.find_by(id: params[:service_id]).availabilities.where(staff_id: params[:staff_id])
     end
     @per_day_availabilities = []
+    debugger
 
     @availabilities.each do |availability|
       daily_start_at = availability.start_at.localtime.to_s.split(' ')[1]
@@ -13,11 +14,13 @@ class AvailabilitiesController < ApplicationController
       day_span = (availability.end_date - availability.start_date).to_i
 
       (0..day_span).each do |day_number|
-        @per_day_availabilities << { title: availability.staff.name,
-                                    start: ((availability.start_date + day_number.days).to_s + 'T' + daily_start_at),
-                                    end: ((availability.start_date + day_number.days).to_s + 'T' + daily_end_at),
-                                    rendering: 'background'
-                                  }
+        unless ((availability.start_date + day_number.days) < Date.today)
+          @per_day_availabilities << { title: availability.staff.name,
+                                      start: ((availability.start_date + day_number.days).to_s + 'T' + daily_start_at),
+                                      end: ((availability.start_date + day_number.days).to_s + 'T' + daily_end_at),
+                                      rendering: 'background'
+                                    }
+        end
       end
 
     end
