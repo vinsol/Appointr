23b67bd203880f7f527010a90e9199ee36230fad
Gaurav <gaurav@vinsol.com>
2014-12-01 13:20:03 +0530

    Disable customer to edit past appointments.

--- app/assets/javascripts/appointment.js
+++ app/assets/javascripts/appointment.js
@@ -57,14 +57,24 @@ LoadCalendar.prototype.initialiseCalendar = function(service_id, staff_id) {
 
         ],
         eventClick: function(calEvent, jsEvent, view) {
-          $.ajax({
-            url: 'appointments/' + calEvent['id'] + '/edit'
-          })
+          if(calEvent['start']['_d'] > (new Date)) {
+            $.ajax({
+              url: 'appointments/' + calEvent['id'] + '/edit'
+            })
+          } else {
+            $.ajax({
+              url: 'appointments/' + calEvent['id']
+            })
+          }
         },
         select: function(start, end, jsEvent, view) {
-          $.ajax({
-            url: 'appointments/new?start=' + start['_d'] + '&end=' + end['_d']
-          })
+          if(start['_d'] > (new Date)) {
+            $.ajax({
+              url: 'appointments/new?start=' + start['_d'] + '&end=' + end['_d']
+            })
+          } else {
+
+          }
         },
         selectOverlap: function(event) {
           return event.rendering === 'background';
@@ -86,69 +96,4 @@ LoadCalendar.prototype.bindEvents = function() {
 $(document).ready(function() {
   var loadCalendar = new LoadCalendar;
   loadCalendar.init()
-  // $('#staff').children().hide()
-  // $('#staff').children().first().show()
-
-  // $('#service').on('change', function() {
-  //   var value = $(this).val();
-  //   var staff_ids = String($("#service option:selected").data('staff_ids')).split(' '),
-  //       _this = this;
-  //   $('#staff').children().hide()
-  //   if(value) {
-  //     $.each(staff_ids, function(index, staff) {
-  //       $('#staff').children('option[value = "' + staff + '"]').show();
-  //     });
-  //     // $('#staff').val($('#staff').children(':visible').first().val())
-
-  //     $('#calendar').fullCalendar('destroy');
-
-  //     $('#calendar').fullCalendar({
-  //       contentHeight: 400,
-  //       aspectRatio: 2,
-  //       defaultView: 'agendaWeek',
-  //       allDaySlot: false,
-  //       selectable: true,
-  //       slotDuration: '00:15:00',
-  //       snapDuration: '00:15:00',
-  //       slotEventOverlap: false,
-  //       header: {
-  //         left: 'prev,next today',
-  //         center: 'title',
-  //         right: 'agendaWeek,agendaDay'
-  //       },
-  //       eventSources: [
-  //         {
-  //           color: 'green',
-  //           url: '/availabilities',
-  //           data: { 'service_id': value },
-  //           textColor: 'blue'
-  //         },
-  //         {
-  //           url: '/appointments',
-  //           color: 'red',
-  //           textColor: 'blue'
-  //         }
-
-  //       ],
-  //       eventClick: function(calEvent, jsEvent, view) {
-  //         $.ajax({
-  //           url: 'appointments/' + calEvent['id'] + '/edit'
-  //         })
-  //       },
-  //       select: function(start, end, jsEvent, view) {
-  //         $.ajax({
-  //           url: 'appointments/new?start=' + start['_d'] + '&end=' + end['_d']
-  //         })
-  //       },
-  //       selectOverlap: function(event) {
-  //         return event.rendering === 'background';
-  //       }
-  //     })
-  //   }
-  //   else {
-  //     $('#staff').children().first().show();
-  //     $('#staff').val('')
-  //     $('#calendar').fullCalendar('destroy');
-  //   }
-  // })
 });
