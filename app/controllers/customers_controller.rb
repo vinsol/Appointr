e65752b57b9b9c3bff5006a5f757ecc8b8c25ed7
Gaurav <gaurav@vinsol.com>
2014-12-04 16:42:21 +0530

    Add an 'or' condition in 'where' clause for inactive appointments in customer controller.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,6 +6,6 @@ class CustomersController < ApplicationController
 
   def history
     @active_appointments = current_customer.appointments.where(state: 'approved').where("start_at > '#{ Time.now }'").order(:start_at).includes(:staff, :service)
-    @inactive_appointments = current_customer.appointments.where.not(state: 'approved').where.not("start_at > '#{ Time.now }'").order(:start_at).includes(:staff, :service)
+    @inactive_appointments = current_customer.appointments.where("state != 'approved' OR start_at <= '#{ Time.now }'").order(:start_at).includes(:staff, :service)
   end
 end
