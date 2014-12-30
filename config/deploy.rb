838d968b37b9fdc3c9defe76c19a3c8f32a6dfb4
Gaurav <gaurav@vinsol.com>
2014-12-30 13:19:12 +0530

    add before_fork and after_fork in unicorn.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :branch, 'as-30'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
